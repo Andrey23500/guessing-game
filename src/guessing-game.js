ed83c0c69b8f2ea65514d52f7e63288110ef42bc
Andrey <andreyavs.00@gmail.com>
2023-01-25 20:44:00 +0300

    feat: add function in guessing-game.js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,21 @@
 class GuessingGame {
-    constructor() {}
+    constructor() { }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
     }
 
     guess() {
-
+        return Math.round((this.minValue + this.maxValue) / 2);
     }
 
     lower() {
-
+        this.maxValue = Math.round((this.minValue + this.maxValue) / 2);;
     }
 
     greater() {
+        this.minValue = Math.round((this.minValue + this.maxValue) / 2);;
 
     }
 }
